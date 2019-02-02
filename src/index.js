18dd8667be36cbf06b5832abfdf01960cf0cc30d
ahmedameenaim <run_2005_2005@yahoo.com>
2019-02-02 13:01:34 +0200

    add redux

--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import Login from './components/auth/login';
 import Register from './components/auth/register';
-import firebase from './firebase';
+import firebase from './firebase'
 import { BrowserRouter as Router, Switch, Route, withRouter } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 import 'semantic-ui-css/semantic.min.css'
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension'
+
+const store = createStore(() => {}, composeWithDevTools())
 
 class Root extends React.Component {
     componentDidMount() {
@@ -29,9 +34,13 @@ class Root extends React.Component {
 
 const RootWithAuth = withRouter(Root)
 
-ReactDOM.render(<Router>
-<RootWithAuth />
-</Router>, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store = {store}>
+        <Router>
+            <RootWithAuth />
+        </Router>
+    </Provider>
+    , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
